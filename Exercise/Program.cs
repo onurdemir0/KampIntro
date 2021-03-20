df64449b1b3ccd0a113665cfe51d380c0d5d7214
Onur Demir <Onur.d77@outlook.com>
2021-03-20 16:09:07 +0300

    Exercise added

---
+++ Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("First number: ");
+			int number1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Second number: ");
+			int number2 = Convert.ToInt32(Console.ReadLine());
+
+			number1 += number2;
+			number2 = number1 - number2;
+			number1 -= number2;
+
+			Console.WriteLine("After swapping: First number: {0} / Second number: {1}", number1, number2);
+		}
+	}
+}
